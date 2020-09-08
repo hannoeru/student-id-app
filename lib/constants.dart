4ebd871267cf2e4fdcdc415228ebe5131755a406
hanlee <me@hanlee.co>
2020-09-09 01:36:51 +0900

    feat: style components

---
+++ lib/constants.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const kNavBarIconColor = Color(0x99ebebf5);
+const kNavBarIconActiveColor = Color(0xffffffff);
+const kSeparateLine = Color(0x7fffffff);
+
+// Text Style
+const kLargeTitleBold = TextStyle(
+  color: Colors.white,
+  fontSize: 33,
+  fontFamily: "SF Pro Display",
+  fontWeight: FontWeight.w700,
+);
+
+const kTitleSemiBold = TextStyle(
+  color: Colors.black,
+  fontSize: 17,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kNavBar = TextStyle(
+  color: Color(0x99ebebf5),
+  fontSize: 24,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w400,
+);
+
+const kIdCardSmall = TextStyle(
+  color: Colors.white,
+  fontSize: 8,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w400,
+);
+
+const kIdCardSmallBold = TextStyle(
+  color: Colors.white,
+  fontSize: 8,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w700,
+);
+
+const kIdCardGraySemibold = TextStyle(
+  color: Color(0xccffffff),
+  fontSize: 10,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kIdCardWhiteSemibold = TextStyle(
+  color: Colors.white,
+  fontSize: 10,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kIdCardTitleSemibold = TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kIdCardTitle2Semibold = TextStyle(
+  color: Colors.white,
+  fontSize: 11,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kNotifyMainTitleSemibold = TextStyle(
+  color: Color(0xb2ffffff),
+  fontSize: 12,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kNotifyTitleSemibold = TextStyle(
+  color: Colors.white,
+  fontSize: 15,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w600,
+);
+
+const kNotifyContant = TextStyle(
+  color: Color(0xb2ffffff),
+  fontSize: 13,
+  fontFamily: "SF Pro Text",
+  fontWeight: FontWeight.w400,
+);
