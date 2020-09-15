7f6ff500a5192cbb8037f7830c3d46567cedff1b
hanlee <me@hanlee.co>
2020-09-15 21:47:06 +0900

    feat: move navtigatin to navbar

--- lib/App.dart
+++ lib/App.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'screens/books_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/schedule_screen.dart';
-import 'screens/settings_screen.dart';
+import 'screens/login_screen.dart';
 import 'widgets/NavBar.dart';
 
 class MyApp extends StatelessWidget {
-  PageController _pageController = PageController();
-  List<Widget> _screens = [
-    HomeScreen(),
-    ScheduleScreen(),
-    BooksScreen(),
-    SettingsScreen()
-  ];
-
-  void _onPageChanged(int index) {}
+  final bool isLoggin = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: PageView(
-          controller: _pageController,
-          children: _screens,
-          onPageChanged: _onPageChanged,
-        ),
-        bottomNavigationBar: NavBar(page: _pageController),
-      ),
+      home: isLoggin ? NavBar() : LoginScreen(),
     );
   }
 }
--- lib/widgets/NavBar.dart
+++ lib/widgets/NavBar.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
 
+import '../screens/books_screen.dart';
+import '../screens/home_screen.dart';
+import '../screens/schedule_screen.dart';
+import '../screens/settings_screen.dart';
+
 import '../constants.dart';
 
 class NavBar extends StatefulWidget {
-  final PageController page;
-  NavBar({@required this.page});
+  NavBar();
 
   @override
   _NavBarState createState() => _NavBarState();
@@ -13,10 +17,15 @@ class NavBar extends StatefulWidget {
 
 class _NavBarState extends State<NavBar> {
   int selectedIndex = 0;
+  List<Widget> _screens = [
+    HomeScreen(),
+    ScheduleScreen(),
+    BooksScreen(),
+    SettingsScreen()
+  ];
 
   void _onNavTapped(int index) {
     setState(() {
-      widget.page.jumpToPage(index);
       selectedIndex = index;
     });
     print(selectedIndex);
@@ -24,56 +33,60 @@ class _NavBarState extends State<NavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Container(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            FlatButton(
-              child: Icon(
-                SFSymbols.person,
-                size: 32.0,
-                color: selectedIndex == 0
-                    ? kNavBarIconActiveColor
-                    : kNavBarIconColor,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _screens[selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              FlatButton(
+                child: Icon(
+                  SFSymbols.person,
+                  size: 32.0,
+                  color: selectedIndex == 0
+                      ? kNavBarIconActiveColor
+                      : kNavBarIconColor,
+                ),
+                onPressed: () => _onNavTapped(0),
               ),
-              onPressed: () => _onNavTapped(0),
-            ),
-            FlatButton(
-              child: Icon(
-                SFSymbols.calendar,
-                size: 32.0,
-                color: selectedIndex == 1
-                    ? kNavBarIconActiveColor
-                    : kNavBarIconColor,
+              FlatButton(
+                child: Icon(
+                  SFSymbols.calendar,
+                  size: 32.0,
+                  color: selectedIndex == 1
+                      ? kNavBarIconActiveColor
+                      : kNavBarIconColor,
+                ),
+                onPressed: () => _onNavTapped(1),
               ),
-              onPressed: () => _onNavTapped(1),
-            ),
-            FlatButton(
-              child: Icon(
-                SFSymbols.book,
-                size: 32.0,
-                color: selectedIndex == 2
-                    ? kNavBarIconActiveColor
-                    : kNavBarIconColor,
+              FlatButton(
+                child: Icon(
+                  SFSymbols.book,
+                  size: 32.0,
+                  color: selectedIndex == 2
+                      ? kNavBarIconActiveColor
+                      : kNavBarIconColor,
+                ),
+                onPressed: () => _onNavTapped(2),
               ),
-              onPressed: () => _onNavTapped(2),
-            ),
-            FlatButton(
-              child: Icon(
-                SFSymbols.slider_horizontal_3,
-                size: 32.0,
-                color: selectedIndex == 3
-                    ? kNavBarIconActiveColor
-                    : kNavBarIconColor,
+              FlatButton(
+                child: Icon(
+                  SFSymbols.slider_horizontal_3,
+                  size: 32.0,
+                  color: selectedIndex == 3
+                      ? kNavBarIconActiveColor
+                      : kNavBarIconColor,
+                ),
+                onPressed: () => _onNavTapped(3),
               ),
-              onPressed: () => _onNavTapped(3),
-            ),
-          ],
+            ],
+          ),
         ),
+        color: Color(0xff262626),
       ),
-      color: Color(0xff262626),
     );
   }
 }
